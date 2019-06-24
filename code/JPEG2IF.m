db5c26686a6e9ad0cc459f91501a9315644c0e1c
CauchyComplete <corundum240@gmail.com>
2019-06-24 16:20:12 +0900

    created directories, made code concise, fixed duplicate frame bug

--- code/JPEG2IF.m
+++ code/JPEG2IF.m
@@ -1,15 +1,13 @@
 function JPEG2IF(input, output, size, factor, gp, bf)
-%-start_number : 인코딩 시작 인덱스 설정 (JPEG 파일의 번호)
-%-c:v: 코덱 설정
+%-start_number : start index (JPEG file number)
+%-c:v: codec option
 %gp  : GOP size
-%-bf : B-frame 수
-%-r  : 프레임 레이트
-%-an : audion 제거
-%y   : 같은 이름의 파일 있을 때 덮어쓰기
+%-bf : B-frame count
+%-r  : frame rate
+%-an : remove audion
+%y   : overwrite if file already exists
+frameRate = 5;
 
-command = strcat('ffmpeg',{' '}, '-start_number 0', {' '}, '-s:v',{' '}, size, {' '}, '-i', {' '},input);
-command = strcat(command,{' '},'-c:v libx264 -x264opts keyint=',int2str(gp),':min-keyint=',int2str(gp),':scenecut=-1');
-command = strcat(command,{' '}, '-r 30', {' '}, '-b:v',{' '}, factor,{' '}, '-bf', {' '}, int2str(bf), {' '}, '-an' ,{' '}, '-y');
-command = strcat(command, {' '},output);
-% command{1};
-system(command{1});
+command = sprintf('ffmpeg -r %d -start_number 1 -s:v %s -i %s -c:v libx264 -x264opts keyint=%d:min-keyint=%d:scenecut=-1 -r %d -b:v %s -bufsize %s -bf %d -an -y %s', frameRate, size, input, gp, gp, frameRate, factor, factor, bf, output);
+% EX: 'ffmpeg -r 30 -start_number 0 -s:v 128x128 -i ..\data\AU_S_%3d.bmp -c:v libx264 -x264opts keyint=1:min-keyint=1:scenecut=-1 -r 30 -b:v 1000k -bf 0 -an -y ..\output\output.mp4'
+system(command);
--- code/main_JPEG2IF.m
+++ code/main_JPEG2IF.m
@@ -1,66 +1,11 @@
 % codec = 'h.264'; codec type-default
 % mode = 'c'; CBR mode-default
 
-input = 'D:\test\JPEG\JPEG%3d.jpg';
-output = 'D:\test\H264\output.mp4';
+input = '..\data\AU_S_%3d.bmp'; %'D:\test\JPEG\JPEG%3d.jpg';
+output = '..\output\output.mp4'; %'D:\test\H264\output.mp4';
 factor = '1000k';
-size = '512x384';
+size = '128x128';
 bf = 0;
 gp = 1;
 
 JPEG2IF(input, output, size, factor, gp, bf);
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-% list = dir(inputdir);
-% for i = 1:length(list)
-%     if(list(i).isdir ~= 1)
-%         filename = list(i).name;
-%         input = strcat(inputdir,'\',filename);
-%         for j = 1:length(factor)
-%             raw2single_crop(video_num, crop_num, input, outputdir, size, codec, mode, factor{j}, gp, bf, crop_length, w_start,h_start);
-%         end
-%         video_num = video_num + 1;
-%     end
-% end
-
-% list = dir(inputdir);
-% for i = 1:length(list)
-%     if(list(i).isdir ~= 1)
-%         filename = list(i).name;
-%         input = strcat(inputdir,'\',filename);
-%         for j = 1:length(factor)
-%             raw2single_crop(video_num, crop_num, input, outputdir, size, codec, mode, factor{j}, gp, bf, crop_length, w_start,h_start);
-%         end
-%         video_num = video_num + 1;
-%     end
-% end
